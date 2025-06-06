cebaedcc4b39fc651c2be92cbf21a10fdbc18178
Martin Kapal <flamecze@gmail.com>
2025-06-06 09:55:04 +0200

    Upgrade to React Node InSim v0.1.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
+import { InSim } from "node-insim";
 import { InSimFlags } from 'node-insim/packets';
 import { createRoot } from 'react-node-insim';
+
 import { App } from "./App";
 
-const root = createRoot({
-  name: 'React InSim',
-  host: '127.0.0.1',
-  port: 29999,
-  adminPassword: '',
-  flags: InSimFlags.ISF_LOCAL,
-});
+const inSim = new InSim();
+
+inSim.connect({
+  IName: 'React InSim',
+  Host: '127.0.0.1',
+  Port: 29999,
+  Admin: '',
+  Flags: InSimFlags.ISF_LOCAL,
+})
+
+const root = createRoot(inSim);
 
 root.render(<App />);
